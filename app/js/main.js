5fad68f057aff8e130e67fc703818c46754c44ba
Сарсенбаев Нурбол <kuma_official@mail.ru>
2017-11-10 12:25:39 +0600

    section-napitki

--- app/js/main.js
+++ app/js/main.js
@@ -23,6 +23,23 @@ $(function() {
         smartSpeed: 500,
         navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">']
     });
-    
+
+    $(".napitki-carousel").owlCarousel({
+        items: 3,
+        nav: true,
+        margin: 30,
+        smartSpeed: 500,
+        navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],        
+        onChanged: function(e) {
+            setTimeout(function() {
+                $(".napitki-carousel .owl-item").removeClass("scale");
+                var item = $(".napitki-carousel .owl-item.active")[1];
+                if(item) {
+                    $(item).addClass("scale");
+                }                    
+            }, 100);
+        }
+    });
+
 });
     

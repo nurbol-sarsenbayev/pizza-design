b7c14e6458963f7f05facb8392180f31e3a59da7
Сарсенбаев Нурбол <kuma_official@mail.ru>
2017-11-10 12:07:56 +0600

    added section-pizza

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,14 @@ $(function() {
         navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">']
     });
 
+    $(".pizza-carousel").owlCarousel({
+        items: 3,
+        nav: true,
+        loop: true,
+        margin: 1,
+        smartSpeed: 500,
+        navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">']
+    });
+    
 });
     
